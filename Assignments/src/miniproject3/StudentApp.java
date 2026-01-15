641319b6ad08d3df1a4894885b775554675073b0
Akash Sharma <128278253+iakashhhh@users.noreply.github.com>
2026-01-15 12:59:01 +0530

    [Akash] : updated mini project 3

--- Assignments/src/miniproject3/StudentApp.java
+++ Assignments/src/miniproject3/StudentApp.java
@@ -4,9 +4,9 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class StudentApp {
-    private static final String URL = "jdbc:mysql://localhost:3306/student_db";
+    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
     private static final String USER = "root";
-    private static final String PASS = "savi2026";
+    private static final String PASS = "Akash@123";
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
