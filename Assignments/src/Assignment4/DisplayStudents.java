70198f1b0814f261ff0ce5ff66614a08533e42f4
Akash Sharma <128278253+iakashhhh@users.noreply.github.com>
2026-01-15 12:52:38 +0530

    [Akash] : Added assignment 4

--- Assignments/src/Assignment4/DisplayStudents.java
+++ Assignments/src/Assignment4/DisplayStudents.java
@@ -7,9 +7,9 @@ import java.sql.Statement;
 
 public class DisplayStudents {
     public static void main(String[] args) {
-        String url = "jdbc:mysql://localhost:3306/student_db";
+        String url = "jdbc:mysql://localhost:3306/mydb";
         String user = "root";
-        String password = "savi2026";
+        String password = "Akash@123";
 
         try {
             Connection con = DriverManager.getConnection(url, user, password);
